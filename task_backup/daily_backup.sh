9131e13bb28c7c6323cedfee91b29b43702d86da
iko90 <hools9090@gmail.com>
2021-12-02 00:28:20 +0200

    add task backup

---
+++ task_backup/daily_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# What to backup
+backup_files="/home/pilya/test"
+
+# Our destination to backup
+dest="/home/pilya/Downloads"
+
+# Create archive filename.
+day=$(date +"%y-%m-%d")
+archive_file="$day.tgz"
+
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+
+
+# Print end status message.
+echo "Backup finished"
+date
+
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
+
+
+find $dest -name '21-12-01.tgz' -mtime +59 -delete 
+
+
